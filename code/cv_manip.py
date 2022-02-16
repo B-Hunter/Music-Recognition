9c57475f09d895f564cc44df522f8bfd1781ddee
Hunter Baum <85764236+B-Hunter@users.noreply.github.com>
2022-02-15 17:16:51 -0700

    Add files via upload

--- code/cv_manip.py
+++ code/cv_manip.py
@@ -2,7 +2,10 @@ import numpy as np
 import cv2
 
 coordinates = []
+
 staff = cv2.imread('staff.png', 1)
+staff2 = cv2.imread('staff.png', 1)
+picWidth = staff.shape[1]
 T = 10
 
 
@@ -20,12 +23,7 @@ def coord_click():
             cv2.putText(staff, str(x) + ',' +
                         str(y), (x, y), font,
                         1, (255, 0, 0), 2)
-            # displaying the coordinates
-            # on the image window
-            # font = cv2.FONT_HERSHEY_SIMPLEX
-            # cv2.putText(staff, str(x) + ',' +
-            #             str(y), (x, y), font,
-            #             1, (255, 0, 0)
+
             cv2.imshow('Staff', staff)
 
             coordinates.append((x, y))
@@ -42,26 +40,53 @@ def coord_click():
 
 
 def buffer_generator(T, image):
+    white_val = [0, 255, 0]
     # loop over the image, pixel by pixel
-    for y in range(0, image.shape[0]):
-        for x in range(0, image.shape[1]):
+    for y in range(image.shape[0]):
+        for x in range(image.shape[1]):
+            top_test_val = image[y, x]
             # threshold the pixel
-            if image[y, x] == 255:
+            if top_test_val[1] == white_val[1]:
                 buff_top = y - T
+                break
+        else:
+            continue
+        break
 
-    for y in reversed(range(0, image.shape[0])):
-        for x in reversed(range(0, image.shape[1])):
+    for y in reversed(range(image.shape[0])):
+        for x in reversed(range(image.shape[1])):
+            bottom_test_val = image[y,x]
             # threshold the pixel
-            if image[y, x] == 255:
+            if bottom_test_val[1] == white_val[1]:
                 buff_bot = y + T
+                break
+        else:
+            continue
+        break
 
     return buff_top, buff_bot
 
 
+
+
+# box iterates over staff line by half-width until end of frame
+def iterate_box(image):
+    [[x, y], [a, b]] = coordinates
+    while a < picWidth:
+        x = int(x + width / 2)
+        a = int(a + width / 2)
+        print(x, y, a, b)
+        next_box = image[y:b, x:a]
+        buffer_generator(15, next_box)
+        cv2.imshow('next', next_box)
+        cv2.waitKey(0)
+
+        return next_box
+
 coord_click()
 
-
 print('Coordinates: ', coordinates)
+
 cv2.rectangle(staff, coordinates[0], coordinates[1], (0, 255, 0), 2)
 
 cv2.imshow('Rectangle', staff)
@@ -73,7 +98,9 @@ cv2.waitKey(0)
 height = snip.shape[0]
 width = snip.shape[1]
 cv2.destroyAllWindows()
+print(picWidth)
+iterate_box(staff2)
+# cv2.imshow('snippet', box)
+# cv2.waitKey(0)
 
-buffer_generator(T, snip)
-block = snip[coordinates]
 exit()
